dbdd101084669e4cea75094c0af5c49cc112bb08
James Snell <vulpes3.1415@yahoo.com>
2023-12-20 18:31:49 -0700

    Found some data

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // Use the data from the response to do DOM manipulation
+    // console.log(data); // Log the data to the console to verify the fetch is working
+    console.log("Book number 5: " + data[4].name)
+    
+    let totalNumberOfPages = 0; // Initialize a variable to store the total number of pages
+
+    for (let index in data) {
+      const book = data[index];
+      const numberOfPages = book.numberOfPages;
+      totalNumberOfPages += numberOfPages;
+    }
+    console.log("Total number of pages in series: " + totalNumberOfPages); // Log the total number of pages
+  });
 }
 
 function renderBooks(books) {
